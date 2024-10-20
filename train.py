f917aa67f6b38c1a424b947deb3352e37b911b84
asugolov <asugolov@v3.cluster.local>
2024-10-19 20:50:07 -0400

    train

--- train.py
+++ train.py
@@ -3,6 +3,18 @@ from vit import ViT
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+ViT_config = {
+    "image_size": 32, 
+    "patch_size": 2, 
+    "num_classes": 10, 
+    "dim": 1024, 
+    "depth": 24, 
+    "heads": 16, 
+    "mlp_dim": 4096, 
+    "channels": 3, 
+    "dim_head": 1024
+}
+
 # additional fn
 
 if __name__ == "__main__":
@@ -34,36 +46,16 @@ if __name__ == "__main__":
 
     if args.skipnorm:
         name += "ViT_SN"
-        model_config = {
-            "image_size": 32, 
-            "patch_size": 4, 
-            "num_classes": 10, 
-            "dim": 256, 
-            "depth": 10, 
-            "heads": 8, 
-            "mlp_dim": 256, 
-            "channels": 3, 
-            "dim_head": 128,
-            "sn_window_size": 4
-        }
+
+        model_config = ViT_config
+        model_config["sn_window_size"] = 4
 
         torch.save(model_config, os.path.join(args.weight_dir, name + f"_model_config"))
         model = ViT(**model_config).to(device)
     else:
         name += "ViT"
 
-        model_config = {
-            "image_size": 32, 
-            "patch_size": 4, 
-            "num_classes": 10, 
-            "dim": 256, 
-            "depth": 10, 
-            "heads": 8, 
-            "mlp_dim": 256, 
-            "channels": 3, 
-            "dim_head": 128
-        }
-
+        model_config = ViT_config
         torch.save(model_config, os.path.join(args.weight_dir, name + f"_model_config"))
         model = ViT(**model_config).to(device)
 
@@ -88,7 +80,28 @@ if __name__ == "__main__":
     # train loop
     wandb.init(project="skipnorm", name=name, config=model_config)
 
-    optimizer = optim.Adam(model.parameters(), lr=2e-4)
+    # optimizer 
+    optimizer = optim.Adam(model.parameters(), lr=6e-4, weight_decay=0.1)
+
+    # define eval loop
+    def eval_loop(model, test_loader, epoch):
+        correct = 0
+        with torch.no_grad():
+            for X, c in tqdm(test_loader):
+
+                X, c = X.to(device), c.to(device)
+                pred = model(X)
+                c_pred = torch.max(pred, dim=1).indices
+                
+                correct += torch.sum(c_pred == c)
+
+            wandb.log({
+                "eval_epoch": epoch,
+                "eval_acc": (correct / n_test_data).item()
+            })
+
+    eval_loop(model, test_loader, 0)
+
     for epoch in range(300):
         
         for X, c in tqdm(train_loader):
@@ -107,36 +120,15 @@ if __name__ == "__main__":
             })   
 
         # eval loop
-        if (epoch + 1) % 1 == 0 or epoch == 0:
-            correct = 0
-            with torch.no_grad():
-                for X, c in tqdm(test_loader):
-
-                    X, c = X.to(device), c.to(device)
-                    pred = model(X)
-                    c_pred = torch.max(pred, dim=1).indices
-                    
-                    correct += torch.sum(c_pred == c)
-
-            wandb.log({
-                "eval_epoch": epoch,
-                "eval_acc": (correct / n_test_data).item()
-            })
-            
+        if (epoch + 1) % 1 == 0:
+            eval_loop(model, test_loader, epoch+1)
 
         # checkpoint
         if (epoch + 1) % 10 == 0:
             torch.save(
                 {"optimizer": optimizer.state_dict(),
                 "model": model.state_dict()},
-                os.path.join(args.weight_dir, name + f"-cpt-{epoch}")
+                os.path.join(args.weight_dir, name + f"-cpt-{epoch+1}")
             ) 
 
     wandb.finish()
-
-
-
-
-
-
-
